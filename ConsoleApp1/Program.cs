466ec5237752f061bb51ff6fb1b01ce434bff624
Petar Kekez <petar.kekez@outlook.com>
2017-06-06 16:55:37 +0200

    test

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -24,7 +24,7 @@ namespace ConsoleApp1
             //}
 
             var rezultati = new List<string>{
-                "Igor Cvitanović"
+                "petar kekez"
             };
 
             var options = new ParallelOptions();
@@ -37,7 +37,7 @@ namespace ConsoleApp1
 
                         int smallWordCount = item.Length > 12 ? 0 : 1;
 
-                        var result = anagram.MultipleWordAnagramWith(item, "tvog", 2);
+                        var result = anagram.MultipleWordAnagramWith(item, "", 1);
 
                         watch.Stop();
                         //var elapsedMs = watch.Elapsed.;
